093b885c87d1a4076da4f8f334e24f33d8012d81
Basti097 <101879144+Basti097@users.noreply.github.com>
2022-10-15 21:21:19 +0200

    L01 final

--- Aufgaben 2. Semester/A01_RandomPoem/poem.js
+++ Aufgaben 2. Semester/A01_RandomPoem/poem.js
@@ -1,17 +1,30 @@
-let subjekt = ["Peter", "Hans", "Georg", "Gertrud", "Uschi", "Sabine"];
-let praedikat = ["isst", "baut", "fährt", "geht", "schneidet", "trinkt"];
-let objekt = ["Pferde", "Blumen", "Hunde", "Gebäude", "Brocken", "Kräuter"];
-for (let _i = subjekt.length; _i > 0; _i--) {
-    getVerse(subjekt, praedikat, objekt);
-}
-function getVerse(_subjekt, _praedikat, _objekt) {
-    let rNumberS = Math.floor(Math.random() * subjekt.length);
-    let rNumberP = Math.floor(Math.random() * praedikat.length);
-    let rNumberO = Math.floor(Math.random() * objekt.length);
-    let randomSubjekt = _subjekt.splice(rNumberS, 1);
-    let randomPraedikat = _praedikat.splice(rNumberP, 1);
-    let randomObjekt = _objekt.splice(rNumberO, 1);
-    console.log(randomSubjekt + " " + randomPraedikat + " " + randomObjekt);
-    return;
-}
+/*
+Aufgabe: L01 Zufallsgedicht
+Name: Bastian Aberle
+Matrikel: 271166
+Datum: 15.10.2022
+Quellen: -
+*/
+var randompoem;
+(function (randompoem) {
+    let subjekt = ["Peter", "Hans", "Georg", "Gertrud", "Uschi", "Sabine"];
+    let praedikat = ["isst", "baut", "fährt", "geht", "schneidet", "trinkt"];
+    let objekt = ["Pferde", "Blumen", "Hunde", "Gebäude", "Brocken", "Kräuter"];
+    for (let _i = subjekt.length; _i > 0; _i--) {
+        getVerse(subjekt, praedikat, objekt);
+    }
+    function getVerse(_subjektP, _praedikatP, _objektP) {
+        let rNumberS = Math.floor(Math.random() * subjekt.length);
+        let rNumberP = Math.floor(Math.random() * praedikat.length);
+        let rNumberO = Math.floor(Math.random() * objekt.length);
+        let randomSubjekt = subjekt.splice(rNumberS, 1);
+        let randomPraedikat = praedikat.splice(rNumberP, 1);
+        let randomObjekt = objekt.splice(rNumberO, 1);
+        console.log(randomSubjekt + " " + randomPraedikat + " " + randomObjekt);
+        console.log(subjekt);
+        console.log(praedikat);
+        console.log(objekt);
+        return;
+    }
+})(randompoem || (randompoem = {}));
 //# sourceMappingURL=poem.js.map
--- Aufgaben 2. Semester/A01_RandomPoem/poem.ts
+++ Aufgaben 2. Semester/A01_RandomPoem/poem.ts
@@ -1,23 +1,34 @@
+/*
+Aufgabe: L01 Zufallsgedicht
+Name: Bastian Aberle
+Matrikel: 271166
+Datum: 15.10.2022
+Quellen: -
+*/
+
 namespace randompoem {
-let subjekt: string[] = ["Peter", "Hans", "Georg", "Gertrud", "Uschi", "Sabine"];
-let praedikat: string[] = ["isst", "baut", "fährt", "geht", "schneidet", "trinkt"];
-let objekt: string[] = ["Pferde", "Blumen", "Hunde", "Gebäude", "Brocken", "Kräuter"];
+    let subjekt: string[] = ["Peter", "Hans", "Georg", "Gertrud", "Uschi", "Sabine"];
+    let praedikat: string[] = ["isst", "baut", "fährt", "geht", "schneidet", "trinkt"];
+    let objekt: string[] = ["Pferde", "Blumen", "Hunde", "Gebäude", "Brocken", "Kräuter"];
 
-for (let _i: number = subjekt.length; _i > 0; _i--) {
-    getVerse(subjekt, praedikat, objekt);
-}
+    for (let _i: number = subjekt.length; _i > 0; _i--) {
+        getVerse(subjekt, praedikat, objekt);
+    }
 
-function getVerse(_subjekt: string[], _praedikat: string[], _objekt: string[]): string {
+    function getVerse(_subjektP: string[], _praedikatP: string[], _objektP: string[]): string {
 
-    let rNumberS: number = Math.floor(Math.random() * subjekt.length);
-    let rNumberP: number = Math.floor(Math.random() * praedikat.length);
-    let rNumberO: number = Math.floor(Math.random() * objekt.length);
+        let rNumberS: number = Math.floor(Math.random() * subjekt.length);
+        let rNumberP: number = Math.floor(Math.random() * praedikat.length);
+        let rNumberO: number = Math.floor(Math.random() * objekt.length);
 
-    let randomSubjekt: string[] = _subjekt.splice(rNumberS, 1);
-    let randomPraedikat: string[] = _praedikat.splice(rNumberP, 1);
-    let randomObjekt: string[] = _objekt.splice(rNumberO, 1);
+        let randomSubjekt: string[] = subjekt.splice(rNumberS, 1);
+        let randomPraedikat: string[] = praedikat.splice(rNumberP, 1);
+        let randomObjekt: string[] = objekt.splice(rNumberO, 1);
 
-    console.log(randomSubjekt + " " + randomPraedikat + " " + randomObjekt);
-    return;
-}
+        console.log(randomSubjekt + " " + randomPraedikat + " " + randomObjekt);
+        console.log(subjekt);
+        console.log(praedikat);
+        console.log(objekt);
+        return;
+    }
 }
